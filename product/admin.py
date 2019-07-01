098fdd9747e8d00ff83f6d2a13f70985439d226e
Manjunath <34913917+sajjanmanju@users.noreply.github.com>
2019-07-01 13:20:07 +0530

    Update admin.py

--- product/admin.py
+++ product/admin.py
@@ -1,24 +1,4 @@
 from django.contrib import admin
 from .models import Product
-from django.contrib.auth.forms import UserCreationForm, UserChangeForm
-from django.contrib.auth.admin import UserAdmin as AuthUserAdmin
-from .models import User
-
-class MyUserCreationForm(UserCreationForm):
-    def clean_username(self):
-        username = self.cleaned_data["username"]
-        try:
-            User._default_manager.get(username=username)
-        except User.DoesNotExist:
-            return username
-        raise forms.ValidationError(self.error_messages['duplicate_username'])
-
-    class Meta(UserCreationForm.Meta):
-        model = User
-
-
-class UserAdmin(AuthUserAdmin):
-    add_product = MyUserCreationForm
-    update_product_class = UserChangeForm
 
 admin.site.register(User,Product)
